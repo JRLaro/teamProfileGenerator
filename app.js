e032b5c9c23eec1597eb8fccf32852c191683c42
J.Laro <joselarojr@gmail.com>
2021-01-21 22:06:50 -0500

    added additional prompt for mananger

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
+const util = require("util");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+// const fileWriter = util.promisify(fs.writeFile);
 
 const render = require("./lib/htmlRenderer");
 
@@ -34,8 +37,20 @@ function init() {
             {
                 type: "input",
                 name: "managerEmail",
-                message: "What is the email of your Manager?"
+                message: "What is the email of your Manager?",
                 //validate user input here
+                validate: function (email) {
+  
+              valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+  
+              if (valid) {
+                  return true;
+              } else {
+                  console.log(".  Please enter a valid email")
+                  return false;
+              }
+          }
+    
             },
             {
                 type: "input",
@@ -43,6 +58,20 @@ function init() {
                 message: "What is the office number of your Manager?"
                 //validate user input here
             },
+            {
+                type: "list",
+                name: "addEmployee",
+                message: "Would you like to add an Employee?",
+                //create a switch statement -> if yes then loop again -> if no proceed to next team-member.
+                choices: [
+                    "Add another Manger",
+                    "Add an Engineer",
+                    "Add an Intern",
+                    "Chill I don't want to add another Employee!!!"
+                ],
+                default: "Chill I don't want to add another Employee!!!",
+            },
+            
         ]).then((answers) => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
             console.log(manager);
@@ -51,9 +80,10 @@ function init() {
             render(teamMembers); // this returns the created HTML
             console.log(render(teamMembers));
 
+            // fileWriter("./test/team.html", render(teamMembers));
+
             function writeToFile(teamMembers) {
-                fs.writeFile('team.html', render(teamMembers), (err) => err ? console.log(err) : console.log('no errors found'));
-            
+               fs.writeFile(outputPath, render(teamMembers), (err) => err ? console.log(err) : console.log('no errors found'));
             }
             writeToFile(teamMembers);
 
@@ -64,13 +94,16 @@ function init() {
 
 init();
 
-
+/*
+THIS IS COMPLETED
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+*/
 
+ 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
